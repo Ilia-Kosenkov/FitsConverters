4e5c7b1edec4e4faae14e962b4f44fd431539fa9
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-03-26 16:37:58 +0200

    Created generic tests

--- tests/Tests/BasicTest.cs
+++ tests/Tests/BasicTest.cs
@@ -1,5 +1,9 @@
+#nullable enable
 using System;
 using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using FitsConverters;
 using FitsCs;
 using ImageCore;
@@ -9,35 +13,41 @@ namespace Tests
 {
     public class BasicTest
     {
-        private IFitsConverter<IImage<float>> _converter;
+        private Random _r;
         [SetUp]
         public void Setup()
         {
-            _converter = ConverterProvider.GetImageConverter(typeof(float)) as IFitsConverter<IImage<float>>;
+            _r = new Random(42);
         }
 
         [Test]
-        public void Test()
+        [GenericTestCase(null!, TypeArguments = new[] { typeof(byte) })]
+        [GenericTestCase(null!, TypeArguments = new[] { typeof(short) })]
+        [GenericTestCase(null!, TypeArguments = new[] { typeof(int) })]
+        [GenericTestCase(null!, TypeArguments = new[] { typeof(long) })]
+        [GenericTestCase(null!, TypeArguments = new[] { typeof(float) })]
+        [GenericTestCase(null!, TypeArguments = new[] { typeof(double) })]
+        public void TestThroughBlocks<T>(object? __ = null) where T : unmanaged, IComparable<T>, IEquatable<T>
         {
             const int n = 248;
             const int m = 496;
-            var data = ArrayPool<float>.Shared.Rent(n * m);
-            
+            var data = ArrayPool<T>.Shared.Rent(n * m);
+            var converter = ConverterProvider.GetImageConverter<T>();
             try
             {
                 for (var i = 0; i < n; i++)
                 for (var j = 0; j < m; j++)
-                    data[m * i + j] = (float)Math.Sqrt(
+                    data[m * i + j] = Internal.UnsafeNumerics.MathOps.DangerousCast<double, T>(Math.Sqrt(
                         (i - 50) * (i - 50) * (1 + Math.Pow(Math.Sin(Math.PI * (i + 23) * 10 / n), 2))
-                        + (j / 2.0 - 100) * (j / 2.0 - 100) * (1.1 + Math.Sin(Math.PI * j * 10 / m)));
+                        + (j / 2.0 - 100) * (j / 2.0 - 100) * (1.1 + Math.Sin(Math.PI * j * 10 / m))));
 
-                data[247 * 496 + 495] = 0f;
-                var src = Image.Create<float>(data.AsSpan(..(n * m)), n, m);
+                data[247 * 496 + 495] = default;
+                var src = Image.Create<T>(data.AsSpan(..(n * m)), n, m);
 
                 var key = FitsKey.Create("DEBUG", "Debug key", "With a comment");
-                var block = _converter.ConvertToBlock(src, key);
+                var block = converter.ConvertToBlock(src, key);
 
-                var rec = _converter.ConvertFromBlock(block, out var keys);
+                var rec = converter.ConvertFromBlock(block, out var keys);
 
                 Assert.AreEqual(src, rec);
                 Assert.AreEqual(data[247 * 496 + 495], src[247, 495]);
@@ -48,7 +58,55 @@ namespace Tests
             }
             finally
             {
-                ArrayPool<float>.Shared.Return(data);
+                ArrayPool<T>.Shared.Return(data);
+            }
+        }
+
+        [Test]
+        [GenericTestCase(null!, TypeArguments = new[] { typeof(byte) })]
+        [GenericTestCase(null!, TypeArguments = new[] { typeof(short) })]
+        [GenericTestCase(null!, TypeArguments = new[] { typeof(int) })]
+        [GenericTestCase(null!, TypeArguments = new[] { typeof(long) })]
+        [GenericTestCase(null!, TypeArguments = new[] { typeof(float) })]
+        [GenericTestCase(null!, TypeArguments = new[] { typeof(double) })]
+        public async Task ReadWriteTest<T>(object? __ = null) where T : unmanaged, IComparable<T>, IEquatable<T>
+        {
+            const int n = 248;
+            const int m = 496;
+            var converter = ConverterProvider.GetImageConverter<T>();
+
+            var data = ArrayPool<T>.Shared.Rent(n * m);
+            try
+            {
+                for (var i = 0; i < n * m; i++)
+                    data[i] = Internal.UnsafeNumerics.MathOps.DangerousCast<double, T>(_r.NextDouble() * i);
+                var src = Image.Create<T>(data.AsSpan(0, n * m), n, m);
+
+
+                await using var fs = new FileStream($"multiple_records_{typeof(T).Name}.fits", FileMode.Create, FileAccess.ReadWrite);
+                await using (var writer = new FitsWriter(fs))
+                {
+                    await converter.WriteToFitsAsync(src, writer);
+                    await converter.WriteToFitsAsync(src, writer, true);
+                }
+                
+                fs.Seek(0, SeekOrigin.Begin);
+                var images = new List<IImage<T>>(2);
+                await using (var reader = new FitsReader(fs))
+                {
+                    await foreach (var block in reader.EnumerateBlocksAsync())
+                    {
+                        images.Add(converter.ConvertFromBlock(block, out _));
+                    }
+                }
+
+                Assert.That(images, Has.Count.EqualTo(2));
+                Assert.AreEqual(src, images[0]);
+                Assert.AreEqual(src, images[1]);
+            }
+            finally
+            {
+                ArrayPool<T>.Shared.Return(data);
             }
         }
     }
---
+++ tests/Tests/GenericTestCaseAttribute.cs
@@ -0,0 +1,41 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Internal.Builders;
+
+namespace Tests
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class GenericTestCaseAttribute : TestCaseAttribute, ITestBuilder
+    {
+        public GenericTestCaseAttribute(params object[] arguments)
+            : base(arguments)
+        {
+        }
+
+
+        public Type[]? TypeArguments { get; set; }
+
+        IEnumerable<TestMethod> ITestBuilder.BuildFrom(IMethodInfo method, Test suite)
+        {
+            if (!method.IsGenericMethodDefinition)
+                return BuildFrom(method, suite);
+
+            if (TypeArguments is null || TypeArguments.Length != method.GetGenericArguments().Length)
+            {
+                var @params = new TestCaseParameters { RunState = RunState.NotRunnable };
+                @params.Properties.Set(@"_SKIPREASON", $"{nameof(TypeArguments)} should have {method.GetGenericArguments().Length} elements.");
+                return new[] { new NUnitTestCaseBuilder().BuildTestMethod(method, suite, @params) };
+            }
+
+            var genMethod = method.MakeGenericMethod(TypeArguments);
+            return BuildFrom(genMethod, suite);
+        }
+    }
+}
